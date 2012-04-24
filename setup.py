8ae4b1116059ce373e2fa9a7cd24b949b423a020
Alex <alexjsgood@gmail.com>
2012-04-24 14:50:08 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name="gocardless",
+        version="0.1.0",
+        author="Alex Good",
+        author_email="alex@gocardless.com",
+        url="http://gocardless.com",
+        description="Python client library for GoCardless",
+        packages=["gocardless"]
+        )
+
