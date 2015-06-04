d3cc795b38c0d5d01763737f5b63ec61a516135e
Nathan Ehresman <n_github@ehresman.org>
2015-06-04 06:21:07 -0400

    Style changes.

--- gocardless/exceptions.py
+++ gocardless/exceptions.py
@@ -11,12 +11,11 @@ class ClientError(GoCardlessError):
 
     def _stringify_errors(self, errors):
         msgs = []
-        if type(errors) == list:
+        if isinstance(errors, list):
             msgs += errors
-        elif type(errors) == dict:
+        elif isinstance(errors, dict):
             for field in errors:
-                msgs += map(lambda m: "{f} {m}".format(f=field, m=m),
-                            errors[field])
+                msgs += ['{} {}'.format(field, msg) for msg in errors[field]]
         else:
             msgs = [str(errors)]
         return ", ".join(msgs)
